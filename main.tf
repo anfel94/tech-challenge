d7180f197dda6cbf737df19acbcb0afe21eb106e
Andres Garcia <104787599+anfel94@users.noreply.github.com>
2025-01-31 22:31:51 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,69 @@
+# google_client_config and kubernetes provider must be explicitly specified like the following.
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = var.project_id
+  name                       = "gke-test-1"
+  region                     = "us-central1"
+  zones                      = ["us-central1-a", "us-central1-b", "us-central1-f"]
+  network                    = "default"
+  subnetwork                 = "default"
+  ip_range_pods              = ""
+  ip_range_services          = ""
+  http_load_balancing        = false
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+
+  node_pools = [
+    {
+      name                       = "default-node-pool"
+      machine_type               = "e2-micro"
+      min_count                  = 1
+      max_count                  = 2
+      disk_size_gb               = 50
+      disk_type                  = "pd-standard"
+      image_type                 = "COS_CONTAINERD"
+      preemptible                = false
+      initial_node_count         = 1
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = []
+
+    default-node-pool = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      node-pool-metadata-custom-value = "my-node-pool"
+    }
+  }
+
+  node_pools_tags = {
+    all = []
+
+    default-node-pool = [
+      "default-node-pool",
+    ]
+  }
+}
